9d19a55afb187f75bb8cf16a2ae91bfde6cd6c46
Derek <derek@Mac.home>
2026-02-26 21:26:21 +0800

    change text

--- src/services/categoryKeyname.js
+++ src/services/categoryKeyname.js
@@ -1,6 +1,6 @@
 export const categoryKeyname = new Map([
-    ['廣告投放', encodeURI('Advertising placement')],
-    ['社群內容', encodeURI('Community content')],
-    ['數位行銷', encodeURI('Digital marketing')],
-    ['電商經營', encodeURI('E-commerce operation')],
+    ['廣告投放', encodeURI('Advertising')],
+    ['社群內容', encodeURI('Social Media')],
+    ['數位行銷', encodeURI('Digital Marketing')],
+    ['電商經營', encodeURI('E-commerce Business')],
 ])
